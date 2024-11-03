4c35577150643090dbf9bda34f676b8810e9f418
Eray Ates <72857634+erayates@users.noreply.github.com>
2024-11-03 23:35:41 +0300

    fix: Deployment issue fixed.

--- app/src/components/theme-toggle.tsx
+++ app/src/components/theme-toggle.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
+// import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -13,7 +13,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  // const { setTheme } = useTheme();}
 
   return (
     <DropdownMenu>
@@ -43,9 +43,7 @@ export function ModeToggle() {
           System
         </DropdownMenuItem>
         */}
-        <DropdownMenuItem>
-          Available Soon
-        </DropdownMenuItem>
+        <DropdownMenuItem>Available Soon</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
